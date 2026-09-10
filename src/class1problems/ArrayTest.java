20ec5f4260a16c65648c49e08c01d8fac22d1dab
Samuraicode3s <roninkva@gmail.com>
2026-09-10 18:55:56 -0400

    finished shiftLeft and shiftRigth methods

--- src/class1problems/ArrayTest.java
+++ src/class1problems/ArrayTest.java
@@ -8,9 +8,14 @@ package class1problems;
 import java.util.Arrays;
 
 public class ArrayTest {
+	
 	public static void leftShift(int[] a) {
+		// 1. Save the first value
+		// 2. Start at index 0
+		// 3. Copy each value from the right into current
+		// 4. Put saved value at the end
+		int first = a[0];
 		if (a.length > 0) {
-			int first = a[0];
 			for (int i = 0; i < a.length - 1; i++) {
 				a[i] = a[i + 1];
 			}
@@ -18,10 +23,28 @@ public class ArrayTest {
 		}
 	}
 
+	public static void rightShift(int[] a) {
+		// 1. Save the last value
+		// 2. Start at the last index
+		// 3. Copy each value from the left into current
+		// 4. Put saved value at index 0
+		int last = a[a.length - 1];
+		if (a.length > 0) {
+			for (int i = a.length - 1; i > 0; i--) {
+				a[i] = a[i - 1];
+			}
+		}
+		a[0] = last;
+	}
+
 	public static void main(String[] args) {
-		int[] arr = { 2, 3, 0, 8, 10 };
-		System.out.println(Arrays.toString(arr));
+		int[] arr = { 1, 2, 3, 4, 5 };
+		int[] arr2 = { 1, 2, 3, 4, 5 };
+		System.out.println("left shift");
 		leftShift(arr);
 		System.out.println(Arrays.toString(arr));
+		System.out.println("right shift");
+		rightShift(arr2);
+		System.out.println(Arrays.toString(arr2));
 	}
 }
